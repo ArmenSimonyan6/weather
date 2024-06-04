572663c7367f53263d8eae0f0fd9b405724f6421
unknown <ArmenSimonyan6>
2024-06-04 17:32:15 +0400

    clear

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const postsAll = document.querySelector(".weather-app")
 const abc = searchButton.addEventListener("click", (e) => {
     e.preventDefault()
 
-    // `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${key}`
 
     searchInput.value
     const requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&units=metric&appid=${key}`
@@ -17,9 +16,11 @@ const abc = searchButton.addEventListener("click", (e) => {
     const data = xhr.response;
     console.log(data);
 
-    let key;
+
+    postsAll.innerHTML = ""
 
     for(let i = 0; i < data.list.length; i++) {
+
         postsAll.innerHTML += `
         
         <div class="weather">
@@ -49,77 +50,9 @@ const abc = searchButton.addEventListener("click", (e) => {
         `
     }
 
-
-    // document.querySelector('.weather-name').textContent = data.city.name
-
-    // document.querySelector('.weather-image').innerHTML = '<img src="https://openweathermap.org/img/wn/'+ data.list[0].weather[0].icon + '.png">'
-
-    // document.querySelector('.weather-celcius').textContent = Math.ceil(data.list[0].main.temp) + " ℃"
-
-    // document.querySelector('.weather-data').textContent = data.list[0].dt_txt
-
-    // document.querySelector('.temp-min').textContent = "Minimal: " + Math.ceil(data.list[0].main.temp_min) + " ℃"
-
-    // document.querySelector('.temp-max').textContent = "Maximal: " + Math.ceil(data.list[0].main.temp_max) + " ℃"
-
-    // document.querySelector(".weather-wind").textContent = "Wind: " + data.list[0].wind.speed + " m/s"    
-
     searchInput.value = ""
+
 }
 xhr.send()
 
 })
-// console.log(abc)
-
-// const requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=Erevan&units=metric&appid=${key}`
-// const xhr = new XMLHttpRequest()
-// xhr.open('GET', requestURL)
-// xhr.responseType = 'json'
-// xhr.onload = async (city) => {
-//     const data = await xhr.response;
-//     console.log(data);
-
-
-//     document.querySelector('.weather-name').textContent = data.city.name
-
-//     document.querySelector('.weather-image').innerHTML = '<img src="https://openweathermap.org/img/wn/'+ data.list[0].weather[0].icon + '.png">'
-
-//     document.querySelector('.weather-celcius').textContent = Math.ceil(data.list[0].main.temp) + " ℃"
-
-//     document.querySelector('.weather-data').textContent = data.list[0].dt_txt
-
-//     document.querySelector('.temp-min').textContent = "Minimal: " + Math.ceil(data.list[0].main.temp_min) + " ℃"
-
-//     document.querySelector('.temp-max').textContent = "Maximal: " + Math.ceil(data.list[0].main.temp_max) + " ℃"
-
-//     document.querySelector(".weather-wind").textContent = "Wind: " + data.list[0].wind.speed + " m/s"
-    
-// }
-// xhr.send()
-
-
-
-// fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Erevan&units=metric&appid=${key}`).then(function (resp) { return resp.json()})
-// .then(function (data) {
-//     console.log(data);
-
-//     let btn = document.querySelector('.weather-name-input')
-    
-
-//     document.querySelector('.weather-name').textContent = data.city.name
-
-//     document.querySelector('.weather-image').innerHTML = '<img src="https://openweathermap.org/img/wn/'+ data.list[0].weather[0].icon + '.png">'
-
-//     document.querySelector('.weather-celcius').textContent = Math.ceil(data.list[0].main.temp) + "℃"
-
-//     document.querySelector('.weather-data').textContent = data.list[0].dt_txt
-
-//     document.querySelector('.temp-min').textContent = "Minimal: " + Math.ceil(data.list[0].main.temp_min) + "℃"
-
-//     document.querySelector('.temp-max').textContent = "Maximal: " + Math.ceil(data.list[0].main.temp_max) + "℃"
-
-//     document.querySelector(".weather-wind").textContent = "Wind: " + data.list[0].wind.speed + " m/s"
-// })
-// .catch(function () {
-    
-// })
